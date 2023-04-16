b6753ef2ff73cfe0a10e959b9561bdbf5ee301ea
phucnv <nguyenvanphucbk8@gmail.com>
2023-04-16 14:09:43 +0700

    master: update home page

---
+++ cart.js
@@ -0,0 +1,14 @@
+var cart = [
+    {
+        product_id: 1,
+        soluong: 10,
+    },
+    {
+        product_id: 2,
+        soluong: 1,
+    },
+    {
+        product_id: 3,
+        soluong: 5,
+    }
+];
